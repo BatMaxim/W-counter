340a01a22c0f24d9e6ccb3f400346a0019c47a5e
Maksim <gato-v@yandex.ru>
2021-09-09 21:07:30 +0300

    Refactored class Path

---
+++ w-counter/w-counter/InputParams.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace w_counter
+{
+    class InputParams
+    {
+        string _path;
+        public string Path
+        {
+            get { return _path; }
+            set 
+            {
+                if(!File.Exists(value))
+                    throw new ArgumentException(
+                          $"'{value}' - такого файла не существует");
+                _path = value; 
+            }
+        }
+    }
+}
