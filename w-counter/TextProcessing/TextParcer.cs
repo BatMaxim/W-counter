77b5ba143a7398f94efeb2d6bf9612c95aad26e1
Maksim <gato-v@yandex.ru>
2021-10-20 13:52:12 +0300

    Added test logic to DLL

---
+++ w-counter/TextProcessing/TextParcer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextProcessing
+{
+    class TextParcer
+    {
+        public static Dictionary<string, int> ParceText(String text)
+        {
+            var pattern = @"([A-Za-zА-Яа-я\-`]+)";
+
+            var words = new Dictionary<string, int>();
+            Console.Write(text);
+            return words;
+        }
+
+    }
+}
--- w-counter/w-counter/FileReader.cs
+++ w-counter/w-counter/FileReader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using TextProcessing;
 
 namespace w_counter
 {
@@ -9,23 +10,9 @@ namespace w_counter
     {
         public static Dictionary<string, int> ReadFromFile(string path)
         {
-            var pattern = @"([A-Za-zА-Яа-я\-`]+)";
-            using var streamReader = new StreamReader(path);
-            var line = String.Empty;
-            var words = new Dictionary<string, int>();
-            while ((line = streamReader.ReadLine()) != null)
-            {
-                foreach (var word in Regex.Matches(line, pattern))
-                {
-                    if (words.ContainsKey(word.ToString().ToLower()))
-                    {
-                        words[word.ToString().ToLower()]++;
-                        continue;
-                    }
 
-                    words.Add(word.ToString().ToLower(), 1);
-                }
-            }
+            TextParcer.ParceText("wqeewq");
+            var words = new Dictionary<string, int>();
             return words;
         }
     }
