e48c51596682052b67034ca425609723be29e2e9
Maksim <gato-v@yandex.ru>
2021-10-21 19:11:01 +0300

    Added timer

--- w-counter/w-counter/Program.cs
+++ w-counter/w-counter/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Linq;
+using System.Diagnostics;
+using System.Threading;
 
 namespace w_counter
 {
     static class Program
     {
+        static void ShowTime(Stopwatch stopWatch)
+        {
+            var ts = stopWatch.Elapsed;
+            var elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
         static void Main(string[] args)
         {
             var inputFilePath = new InputParams();
@@ -18,9 +28,12 @@ namespace w_counter
                 Console.WriteLine(exception);
                 return;
             }
-
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
             var words = FileReader.ReadFromFile(inputFilePath.Path);
             if(FileWriter.WriteInFile(inputFilePath.Path, words)) Console.WriteLine("Запись выполнена");
+            stopWatch.Stop();
+            ShowTime(stopWatch);
         }
     }
 }
