7d94b09f9c37085d89e9e142333591d0e097ed91
Maksim <gato-v@yandex.ru>
2021-10-24 19:44:33 +0300

    Changed to .Net Framework

---
+++ w-counter/wCounter/FileWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace wCounter
+{
+    class FileWriter
+    {
+        public static bool WriteInFile(string path, Dictionary<string, int> words, string flag = "")
+        {
+            var newPath = GenerateFilePath(path, flag);
+            using (var streamWriter = new StreamWriter(newPath))
+            {
+                foreach (var item in words.OrderByDescending(item => item.Value))
+                {
+                    streamWriter.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
+            return true;
+        }
+        static string GenerateFilePath(string path, string flag)
+        {
+            var mainPath = path.Remove(path.LastIndexOf("."));
+            var NameMode = DateTime.Now.ToString("yyyyMMddHHmmss");
+            return $"{mainPath}_{NameMode}{flag}.txt" ;
+        }
+    }
+}
---
+++ w-counter/wCounter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilePath = new InputParams();
+            Console.Write("Введите путь входного файла: ");
+            try
+            {
+                inputFilePath.Path = Console.ReadLine();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                return;
+            }
+
+            var words = FileReader.ReadFromFile(inputFilePath.Path, true);
+            if (FileWriter.WriteInFile(inputFilePath.Path, words, "_MT")) Console.WriteLine("Запись выполнена");
+            words = FileReader.ReadFromFile(inputFilePath.Path, false);
+            if (FileWriter.WriteInFile(inputFilePath.Path, words)) Console.WriteLine("Запись выполнена");
+        }
+    }
+}
